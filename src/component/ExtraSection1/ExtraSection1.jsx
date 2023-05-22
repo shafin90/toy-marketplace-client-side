33579a12463b9b9b3eb28024e1958bdcb092278b
shafin90 <mashrafiahnam1@gmail.com>
2023-05-22 08:14:05 -0700

    extra section 2

---
+++ src/component/ExtraSection1/ExtraSection1.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const ExtraSection1 = () => {
+    return (
+        <div className='container my-5'>
+            <div className="row">
+                <div className="col-12 col-md-6">
+                    <h1 className='my-3'>
+                        The Car Toy Economy: Inspiring Fun and Development
+                    </h1>
+                    <article>
+                        The car toy industry holds a prominent place in the global toy market, captivating children and collectors worldwide. These miniature vehicles have a significant impact on child development, fostering imagination, creativity, and fine motor skills. From basic playsets to advanced remote-controlled cars, car toys offer a range of options for all budgets and interests. Moreover, they have educational value, introducing STEM concepts and promoting early interest in science and engineering. Beyond their impact on children, car toys drive a thriving economy, generating revenue, employment opportunities, and a vibrant collector's market. With a focus on sustainability, the industry continues to evolve, shaping the future of play and exploration.
+                    </article>
+                </div>
+                <div className="col-12 col-md-6 d-flex justify-content-center align-items-center">
+                    <img className='img-fluid rounded' src="https://thumbs.dreamstime.com/b/toy-car-children-s-exhibition-sales-shenzhen-china-59953163.jpg" alt="" />
+                </div>
+            </div>
+
+
+        </div>
+    );
+};
+
+export default ExtraSection1;
--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,4 +1,6 @@
 import Banner from "../Banner/Banner";
+import ExtraSection1 from "../ExtraSection1/ExtraSection1";
+import ExtraSection2 from "../ExtraSection2/ExtraSection2";
 import Gallery from "../Gallery/Gallery";
 import PageTitle from "../PageTitle/PageTitle";
 import ReactTab from "../ReactTab/ReactTab";
@@ -12,6 +14,8 @@ const Home = () => {
             <Banner></Banner>
             <Gallery></Gallery>
             <ReactTab></ReactTab>
+            <ExtraSection1></ExtraSection1>
+            <ExtraSection2></ExtraSection2>
             
             
         </div>
