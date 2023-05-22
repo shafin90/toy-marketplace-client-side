2820d0e966bc6704f3b80b4efc40c2de7db6ac29
shafin90 <mashrafiahnam1@gmail.com>
2023-05-22 07:03:38 -0700

    blog added

--- src/component/Blog/Blog.jsx
+++ src/component/Blog/Blog.jsx
@@ -3,9 +3,41 @@ import PageTitle from '../PageTitle/PageTitle';
 
 const Blog = () => {
     return (
-        <div>
-             <PageTitle title={"Blog"}></PageTitle>
-            
+        <div className='container my-5'>
+            <PageTitle title={"Blog"}></PageTitle>
+            <h1>1.What is an access token and refresh token? How do they work and where should we store them on the client-side?
+            </h1>
+            <article>
+
+                An access token is a credential issued to a user after successful authentication. It grants access to protected resources and is typically short-lived. It is used to authorize and perform actions on behalf of the authenticated user.
+
+                A refresh token is also issued during authentication and is used to obtain a new access token when the current one expires. Refresh tokens are long-lived and stored securely on the client-side.
+
+                Both tokens should be stored securely on the client-side, with access tokens typically stored in memory or browser storage, and refresh tokens stored in a more persistent storage like secure cookies or encrypted local storage.
+            </article>
+
+
+
+            <h1>2.Compare SQL and NoSQL databases?</h1>
+            <article>
+
+                SQL databases are based on a structured model with predefined schemas and use SQL for data manipulation. They offer strong data integrity and are suitable for complex relationships and large-scale applications. On the other hand, NoSQL databases are schema-flexible, designed for handling unstructured data, and provide high scalability for large data volumes and high traffic loads. They offer more flexibility but sacrifice some of the strict data integrity guarantees of SQL databases.
+            </article>
+
+
+            <h1>3.What is express js? What is Nest JS?</h1>
+            <article>
+                Express.js is a minimalistic web application framework for Node.js, while Nest.js is a progressive and extensible framework built on top of Express.js with additional features and architectural patterns.
+            </article>
+
+
+            <h1>
+                4.What is MongoDB aggregate and how does it work?
+            </h1>
+            <article>
+                MongoDB Aggregation is a framework for performing data processing operations on MongoDB collections. It allows you to process and transform data using various stages such as filtering, grouping, sorting, and aggregating. Aggregation pipeline consists of multiple stages that can be chained together to perform complex data manipulations. Each stage receives input from the previous stage, processes the data, and passes it to the next stage until the final result is obtained. Aggregation provides powerful capabilities for data analysis, reporting, and generating aggregated results based on specified criteria.
+            </article>
+
         </div>
     );
 };
--- src/component/ReactTab/ReactTab.jsx
+++ src/component/ReactTab/ReactTab.jsx
@@ -3,50 +3,63 @@ import { Tab, Tabs, Container, Row, Col } from 'react-bootstrap';
 import MyCard from '../MyCard/MyCard';
 import './ReactTab.css';
 import { AuthContext } from '../Provider/Provider';
+import Spinner from 'react-bootstrap/Spinner';
 
 const ReactTab = () => {
-  const { regulerCar, sportsCar, trucks } = useContext(AuthContext);
+  const { regulerCar, sportsCar, trucks, data } = useContext(AuthContext);
 
-  console.log(regulerCar);
 
-  return (
-    <Container>
-      <h1 className="text-center">Shop By Category</h1>
-      <Row className="my-5">
-        <Col className='tab_container'>
-          <Tabs
-            defaultActiveKey="regular"
-            className="d-flex justify-content-center align-items-center"
-            id="car-tabs"
-          >
-            <Tab eventKey="regular" title="Regular Car">
-              <div className="grid">
-                {regulerCar.map((e) => (
-                  <MyCard info={e} key={e._id}></MyCard>
-                ))}
-              </div>
-            </Tab>
+  if (!data) {
+    return (
+      <div className='d-flex justify-content-center align-items-center my-5 py-5'>
+        <Spinner animation="border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
+      </div>
+    );
+  }
+  else {
 
-            <Tab eventKey="truck" title="Truck">
-              <div className="grid">
-                {trucks.map((e) => (
-                  <MyCard info={e} key={e._id}></MyCard>
-                ))}
-              </div>
-            </Tab>
+    return (
+      <Container>
+        <h1 className="text-center">Shop By Category</h1>
+        <Row className="my-5">
+          <Col className='tab_container'>
+            <Tabs
+              defaultActiveKey="regular"
+              className="d-flex justify-content-center align-items-center"
+              id="car-tabs"
+            >
+              <Tab eventKey="regular" title="Regular Car">
+                <div className="grid">
+                  {regulerCar.map((e) => (
+                    <MyCard info={e} key={e._id}></MyCard>
+                  ))}
+                </div>
+              </Tab>
 
-            <Tab eventKey="sports" title="Sports Car">
-              <div className="grid">
-                {sportsCar.map((e) => (
-                  <MyCard info={e} key={e._id}></MyCard>
-                ))}
-              </div>
-            </Tab>
-          </Tabs>
-        </Col>
-      </Row>
-    </Container>
-  );
-};
+              <Tab eventKey="truck" title="Truck">
+                <div className="grid">
+                  {trucks.map((e) => (
+                    <MyCard info={e} key={e._id}></MyCard>
+                  ))}
+                </div>
+              </Tab>
 
+              <Tab eventKey="sports" title="Sports Car">
+                <div className="grid">
+                  {sportsCar.map((e) => (
+                    <MyCard info={e} key={e._id}></MyCard>
+                  ))}
+                </div>
+              </Tab>
+            </Tabs>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+
+
+}
 export default ReactTab;
